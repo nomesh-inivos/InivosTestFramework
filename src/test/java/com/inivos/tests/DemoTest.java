8cf7290c3b2c961be3cc68406f8bde7be10183a1
nomesh_inivos <nomesh.desilva@inivossl.com>
2022-09-23 08:57:31 +0530

    changed the project structure by adding java source folder and making test folder as test source root

--- src/test/java/com/inivos/tests/DemoTest.java
+++ src/test/java/com/inivos/tests/DemoTest.java
@@ -1,19 +1,14 @@
 package com.inivos.tests;
 
+
+
 import com.inivos.config.BrowserStackConfigfactory;
-import com.inivos.config.ConfigurationFactory;
-import com.inivos.config.FrameworkConfig;
-import com.inivos.enums.BrowserType;
-import org.aeonbits.owner.ConfigCache;
 import org.testng.annotations.Test;
-import static org.aeonbits.owner.ConfigCache.getOrCreate;
-
-
 public class DemoTest {
+
+
     @Test
     public void testLogin(){
-
-         System.out.println(BrowserStackConfigfactory.getConfig().browserStackUrl());
-
+        System.out.println(BrowserStackConfigfactory.getConfig().browserStackUrl());
     }
 }
